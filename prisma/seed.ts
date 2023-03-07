647ce92961594eecbda981712f15b6d13aab738c
Mahmud Hussain <mahmud.hussain@digital.cabinet-office.gov.uk>
2023-03-07 15:21:40 +0000

    Seed data for mosque and prayer

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -25,6 +25,56 @@ async function main() {
       })
   
   console.log(alice, bob)
+
+  const mosque = await prisma.mosque.create({
+    data: {
+      name: 'Mosque 1',
+      address: {
+        addressLineOne: '123 East St',
+        city: 'London',
+        postcode: 'E1',
+      },
+      email: 'mosque1@example.com',
+      phoneNumber: '555-555-1234',
+    },
+  });
+
+  const prayerTime = await prisma.prayerTime.create({
+    data: {
+      day: 1,
+      month: 3,
+      prayerTimes: {
+        fajr: {
+          start: '5:30 am',
+          jamaah: '6:00 am',
+        },
+        sunrise: '6:30 am',
+        zuhr: {
+          start: '12:00 pm',
+          jamaah: '1:00 pm',
+        },
+        asr: {
+          start: '3:30 pm',
+          jamaah: '4:00 pm',
+        },
+        maghrib: {
+          start: '6:00 pm',
+          jamaah: '6:15 pm',
+        },
+        isha: {
+          start: '8:00 pm',
+          jamaah: '8:30 pm',
+        },
+      },
+      Mosque: {
+        connect: {
+          id: mosque.id,
+        },
+      },
+    },
+  });
+
+  console.log(prayerTime);
 }
 
 main()
